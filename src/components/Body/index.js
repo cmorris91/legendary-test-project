35fdd9fabb89dcddbe88802221f02441d357ba2a
Chelsey Morris <chelseymorris91@yahoo.com>
2022-02-14 16:07:15 -0800

    changed background

--- src/components/Body/index.js
+++ src/components/Body/index.js
@@ -1,8 +1,5 @@
 import React from "react";
 import Header from "../header";
-import { Container } from "react-bootstrap";
-import { Row } from "react-bootstrap";
-import { Col } from "react-bootstrap";
 import "./style.css";
 
 function Body() {
