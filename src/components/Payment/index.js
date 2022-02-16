e815651bcaab3bab0df2eb571d1f96a445e4b642
Chelsey Morris <chelseymorris91@yahoo.com>
2022-02-15 22:30:15 -0800

    added discountAPI

--- src/components/Payment/index.js
+++ src/components/Payment/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link, withRouter } from "react-router-dom";
 import { Input } from "../Forms";
 import { Button } from "../Forms";
@@ -11,10 +11,34 @@ function Payment() {
         cvc: "",
     });
 
+    const [discountCode, setDiscountCode] = useState({
+        code: "",
+    });
+
+    useEffect (() => {
+        fetch('https://api-test.legendaryapplications.com/action/promos/check', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+              body: JSON.stringify({ code: discountCode.code })
+            })
+              .then(data => {
+                const validate = data.json()
+                console.log(validate)
+              })
+
+              
+    }, [handleFormSubmit2]);
+
     function handleInputChange(event) {
         const { name, value } = event.target;
         setPaymentForm({...paymentForm, [name]: value})
-      
+      };
+
+      function handleInputChange2(event) {
+        const { name, value } = event.target;
+        setDiscountCode({...discountCode, [name]: value})
       };
 
       function handleFormSubmit(event) {
@@ -29,32 +53,50 @@ function Payment() {
         }
     };
 
+    function handleFormSubmit2(event) {
+        event.preventDefault();
+        localStorage.setItem("discountcode", discountCode.code);
+    };
+
     return (
         <div>
-            <form>
-                <Input
-                    onChange= {handleInputChange}
-                    name="cardNumber"
-                    value={paymentForm.cardNumber}
-                    placeholder= "Credit Card Number (required)"/> 
+            <div>
+                <form>
+                    <Input
+                        onChange= {handleInputChange}
+                        name="cardNumber"
+                        value={paymentForm.cardNumber}
+                        placeholder= "Credit Card Number (required)"/> 
 
-                <Input
-                    onChange= {handleInputChange}
-                    name="expiration"
-                    value={paymentForm.expiration}
-                    placeholder= "expiration date (required)"/> 
-                <Input
-                    onChange= {handleInputChange}
-                    name="cvc"
-                    value={paymentForm.cvc}
-                    placeholder= "CVC Number (required)"/> 
-                <Button
+                    <Input
+                        onChange= {handleInputChange}
+                        name="expiration"
+                        value={paymentForm.expiration}
+                        placeholder= "expiration date (required)"/> 
+                    <Input
+                        onChange= {handleInputChange}
+                        name="cvc"
+                        value={paymentForm.cvc}
+                        placeholder= "CVC Number (required)"/> 
+                    <Button
+                        name="Submit"
+                        placeholder="Submit"
+                        onClick={handleFormSubmit}>Submit Payment</Button>
+                </form>
+            </div>
+            <div>
+                <form>
+                    <Input
+                    onChange= {handleInputChange2}
+                    name="code"
+                    value={discountCode.code}
+                    placeholder= "enter discount code"/> 
+                    <Button
                     name="Submit"
                     placeholder="Submit"
-                    onClick={handleFormSubmit}>Submit Payment</Button>
-            </form>
-
-            
+                    onClick={handleFormSubmit2}>Submit</Button>
+                </form>
+            </div>
         </div>
     )
 }
